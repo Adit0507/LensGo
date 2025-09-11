0ebd4faf68440a834f33fd43b47e29055e377a9c
Adit0507 <adi05072002@gmail.com>
2025-09-11 23:12:09 +0530

    working on it

--- internal/services/processor.go
+++ internal/services/processor.go
@@ -1,7 +1,52 @@
 package services
 
+import (
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+
+	"strings"
+)
+
 type ImageProcessor struct{}
 
-func NewImageProcessor() *ImageProcessor{
+func NewImageProcessor() *ImageProcessor {
 	return &ImageProcessor{}
+}
+
+// loading image from specified path
+func (p *ImageProcessor) LoadImage(filepath string) (image.Image, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+func (p *ImageProcessor) SaveImage(img image.Image, filePath string) error{
+	file, err := os.Create(filePath)
+	if err !=nil{
+		return err
+	}
+	defer file.Close()
+
+	ext := strings.ToLower(filepath.Ext(filePath))
+
+	switch ext{
+		case ".jpg", ".jpeg":
+		return jpeg.Encode(file, img, &jpeg.Options{Quality: 90})
+	case ".png":
+		return png.Encode(file, img)
+	default:
+		return jpeg.Encode(file, img, &jpeg.Options{Quality: 90})
+	}
 }
--- internal/services/worker_pool.go
+++ internal/services/worker_pool.go
@@ -43,6 +43,75 @@ func (wp *WorkerPool) Stop() {
 	log.Println("Worker pool stopped")
 }
 
-func (wp *WorkerPool) worker(id int) {
+func (wp *WorkerPool) SubmitJob(job models.ProcessingJob) {
+	wp.jobQueue <- job
+}
 
+func (wp *WorkerPool) worker(id int) {
+	defer wp.wg.Done()
+
+	log.Printf("Worker %d started", id)
+
+	for {
+		select {
+		case job := <-wp.jobQueue:
+			log.Printf("Worker %d processing job %s", id, job.ID)
+			result := wp.processJob(job)
+			job.ResultChan <- result
+
+		case <-wp.quit:
+			log.Printf("Worker %d stopping", id)
+			return
+		}
+	}
+}
+
+// handling image processing
+func (wp *WorkerPool) processJob(job models.ProcessingJob) models.ProcessingResult {
+	result := models.ProcessingResult{
+		Success:    true,
+		OutputPath: job.OutputPath,
+	}
+
+	img := job.Image
+	var err error
+
+	if img == nil && job.InputPath != "" {
+		img, err = wp.processor.LoadImage(job.InputPath)
+		if err != nil {
+			result.Success = true
+			result.Error = err
+
+			return result
+		}
+	}
+
+	for _, op := range job.Operations {
+		switch op.Type {
+		case models.OpResize:
+			width := int(op.Params["width"].(float64))
+			height := int(op.Params["height"].(float64))
+			img = wp.processor.Resize(img, width, height)
+
+		case models.OpGrayScale:
+			img = wp.processor.Grayscale(img)
+
+		case models.OpBlur:
+			radius := 2.0
+			if r, ok := op.Params["radius"]; ok {
+				radius = r.(float64)
+			}
+
+			img = wp.processor.Blur(img, radius)
+		}
+	}
+
+	if err := wp.processor.SaveImage(img, job.OutputPath); err != nil {
+		result.Success = false
+		result.Error = err
+
+		return result
+	}
+
+	return result
 }
